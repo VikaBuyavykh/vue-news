a3e773a77e9283b341f3ee4721f7cdf9e115c23c
Vika Buyavykh <vikabuyavykh@gmail.com>
2024-04-10 16:32:45 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup></script>
+
+<template>
+  <h1 class="title">Вика</h1>
+</template>
+
+<style lang="scss">
+@import './assets/fonts/font.scss';
+@import './assets/styles/variables.scss';
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.title {
+  color: $accent-yellow;
+}
+</style>
